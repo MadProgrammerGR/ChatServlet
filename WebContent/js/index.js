40acc429afceb0cc2160fe9a164ee59b6347f49b
MadProgrammerGR <giorgozombo@yahoo.gr>
2017-05-13 22:35:32 +0300

    testing adding message

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -1,17 +1,16 @@
-(function() {
-  var app = angular.module('chatApp', []);
+var app = angular.module('chatApp', []);
 
-  app.controller('MessageCtrl', function($scope) {
-    $scope.messages = [{
-      Name: 'George Clooney',
-      Message: "The only failure is not to try"
-    }, {
-      Name: 'Seth Rogen',
-      Message: "I grew up in Vancouver, man. That's where more than half of my style comes from."
-    }, {
-      Name: 'John Lydon',
-      Message: "There's nothing glorious in dying. Anyone can do it."
+app.controller('MessageCtrl', function($scope) {
+	$scope.messages = [{
+		Name: 'George Clooney',
+		Message: "The only failure is not to try"
     }];
-  });
+});
 
-})();
+function addMessage() {
+	var msg = document.createElement("div");
+	msg.className = "chatbox__messages__user-message--ind-message";
+	msg.innerHTML = "<p class='name'>" + "Giorgos"
+			+ "</p><br/><p class='message'>" + "Hellooo11" + "</p>";
+	document.getElementById("chat_panel").appendChild(msg);
+}
