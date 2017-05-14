9e81d1a7a849d9ed1e354946c4d3d3ca4878f63a
MadProgrammerGR <giorgozombo@yahoo.gr>
2017-05-14 13:55:51 +0300

    making post message on enter/click

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -1,3 +1,5 @@
+var nickname;
+
 function addMessageToChat(user, msg, date) {
 	var li = document.createElement("li");
 	li.className = "media media-body";
@@ -11,3 +13,23 @@ function addUserToUserList(name, time) {
 	li.innerHTML = "<h5>"+name+"</h5><small class='text-muted'>Online For "+time+"</small>";
 	document.getElementById("user_list").appendChild(li);
 }
+
+$(document).ready(function(){
+    do{
+    	nickname = prompt("Please enter a nickname");
+    }while(nickname.trim() == "")
+	
+	$("#input_text").keyup(function(event) {
+	    if(event.keyCode == 13) { //enter key
+	    	sendMessage();
+	    }
+	});
+});
+
+function sendMessage() {
+	var msg = document.getElementById("input_text").value;
+	if(msg.trim() == "") return;
+	document.getElementById("input_text").value = "";
+	$.post("ChatServlet",{name: nickname, message: msg});
+}
+
---
+++ src/ChatServlet.java
@@ -0,0 +1,25 @@
+
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ChatServlet
+ */
+@WebServlet("/ChatServlet")
+public class ChatServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println(request.getParameter("name"));
+		System.out.println(request.getParameter("message"));
+
+	}
+
+}
